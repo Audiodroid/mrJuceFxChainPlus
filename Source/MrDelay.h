6211279c8b722ade52d19cc6a2f8d6ba92b425a5
Audiodroid <marcel.roth76@gmail.com>
2022-02-09 10:57:10 +0000

    split process into read and write

--- Source/MrDelay.h
+++ Source/MrDelay.h
@@ -1,8 +1,8 @@
 /*
   ==============================================================================
 
-   This file is code written by Marcel Roth.   
-   
+   This file is code written by Marcel Roth.
+
    THE CODE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES, WHETHER
    EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR PURPOSE, ARE
    DISCLAIMED.
@@ -17,133 +17,143 @@
 #include <JuceHeader.h>
 
 /**
-    Applies a simple delay to AudioBlocks.
+	Applies a simple delay to AudioBlocks.
 
-    The code is meant to follow the JUCE coding standard
-    https://juce.com/discover/stories/coding-standards
+	The code is meant to follow the JUCE coding standard
+	https://juce.com/discover/stories/coding-standards
 */
 template <typename FloatType>
 class MrDelay
 {
 public:
-    const FloatType SAMPLERATE_DEFAULT = 48000;
-    const FloatType FEEDBACK_DEFAULT = 0.5f;
+	const FloatType SAMPLERATE_DEFAULT = 48000;
+	const FloatType FEEDBACK_DEFAULT = 0.5f;
 
-    MrDelay() noexcept = default;
-    
-    //==============================================================================
-    /* Applies a new feedback value to delay. */
-    void setFeedback(FloatType feedbackNew)
-    {
-        feedback.setTargetValue(feedbackNew);
-    }
+	MrDelay() noexcept = default;
 
-    /* Returns the current feedback value. */
-    FloatType getFeedback() { return feedback.getTargetValue(); }
+	//==============================================================================
+	/* Applies a new feedback value to delay. */
+	void setFeedback(FloatType feedbackNew)
+	{
+		feedback.setTargetValue(feedbackNew);
+	}
 
-    /** Applies new delay as number of samples. */
-    void setDelayInSmpls(size_t delayInSmplsNew) noexcept
-    {
-        delayInSmpls = delayInSmplsNew;
+	/* Returns the current feedback value. */
+	FloatType getFeedback() { return feedback.getTargetValue(); }
 
-        if (dlyBufs == nullptr)
-            return;
+	/** Applies new delay as number of samples. */
+	void setDelayInSmpls(size_t delayInSmplsNew) noexcept
+	{
+		delayInSmpls = delayInSmplsNew;
 
-        dlyBufs->setSize(numChnls, (int) delayInSmpls + 1);
-        dlyBufs->clear();
+		reset();
+	}
 
-        posR = 0;
-        posW = delayInSmpls;
-    }
+	void reset() {
 
-    /** Applies new delay as a millisecond value. */
-    void setDelayInMs(FloatType delayInMs) noexcept { setDelayInSmpls(msToSmpls(delayInMs)); }
+		dlyBufs.setSize(numChnls, (int)delayInSmpls + 1);
+		dlyBufs.clear();
 
-    /** Returns the current delay in a number of samples. */
-    size_t getDelayInSmpls() const noexcept { return delayInSmpls; }
+		posR = 0;
+		posW = delayInSmpls;
+	}
 
-    /** Returns the current delay as a millisecond value. */
-    FloatType getDelayInMs() const noexcept { return smplsToMs(getDelayInSmpls()); }
+	/** Applies new delay as a millisecond value. */
+	void setDelayInMs(FloatType delayInMs) noexcept { setDelayInSmpls(msToSmpls(delayInMs)); }
 
-    /** Converts time in ms to samples */
-    size_t msToSmpls(FloatType ms) const noexcept { return (size_t) (std::round(ms * sampleRate) / 1000.0f); }
+	/** Returns the current delay in a number of samples. */
+	size_t getDelayInSmpls() const noexcept { return delayInSmpls; }
 
-    /** Converts samples to time in ms*/
-    FloatType smplsToMs(size_t smpls) const noexcept { return (sampleRate != 0) ? (smpls * 1000 / sampleRate) : 0.0; }
+	/** Returns the current delay as a millisecond value. */
+	FloatType getDelayInMs() const noexcept { return smplsToMs(getDelayInSmpls()); }
 
-    //==============================================================================
-    /** Called before processing starts. */
-    void prepare(const juce::dsp::ProcessSpec& spec) noexcept
-    {
-        if (sampleRate <= 0)
-            return;
-        
-        auto delayInSmplsNew = (size_t) (std::round((delayInSmpls * spec.sampleRate) / sampleRate));
-        setDelayInSmpls(delayInSmplsNew);
-       
-        sampleRate = spec.sampleRate;
-        numChnls = spec.numChannels;
-        
-        dlyBufs = std::unique_ptr< juce::AudioBuffer<FloatType>>(new juce::AudioBuffer<FloatType>(numChnls, (int) delayInSmpls+1));
-    }
+	/** Converts time in ms to samples */
+	size_t msToSmpls(FloatType ms) const noexcept { return (size_t)(std::round(ms * sampleRate) / 1000.0f); }
 
-    //==============================================================================
-    /** Processes the input and output buffers supplied in the processing context. */
-    template <typename ProcessContext>
-    void process(const ProcessContext& context) noexcept
-    {
-        auto&& inBlock = context.getInputBlock();
-        auto&& outBlock = context.getOutputBlock();
+	/** Converts samples to time in ms*/
+	FloatType smplsToMs(int smpls) const noexcept { return (sampleRate != 0) ? (smpls * 1000 / sampleRate) : 0.0; }
 
-        jassert(inBlock.getNumChannels() == outBlock.getNumChannels());
-        jassert(inBlock.getNumSamples() == outBlock.getNumSamples());
+	//==============================================================================
+	/** Called before processing starts. */
+	void prepare(const juce::dsp::ProcessSpec& spec) noexcept
+	{
+		if (sampleRate <= 0)
+			return;
 
-        auto len = inBlock.getNumSamples();
-        auto numChannels = inBlock.getNumChannels();
+		auto delayInSmplsNew = static_cast<size_t>(std::round((delayInSmpls * spec.sampleRate) / sampleRate));
 
-        if (context.isBypassed)
-        {            
-            if (context.usesSeparateInputAndOutputBlocks())
-                outBlock.copyFrom(inBlock);
+		sampleRate = spec.sampleRate;
+		numChnls = spec.numChannels;
 
-            return;
-        }
+		setDelayInSmpls(delayInSmplsNew);
+	}
 
-        int dlyBufsLen = dlyBufs->getNumSamples();
-        for (size_t pos = 0; pos < len; ++pos, ++posR, ++posW)
-        {
-            if (posR >= dlyBufsLen)
-                posR = 0;
+	//==============================================================================
+	/** Processes the input and output buffers supplied in the processing context. */
+	template <typename ProcessContext>
+	void process(const ProcessContext& context) noexcept
+	{
+		auto&& inBlock = context.getInputBlock();
+		auto&& outBlock = context.getOutputBlock();
 
-            if (posW >= dlyBufsLen)
-                posW = 0;
+		jassert(inBlock.getNumChannels() == outBlock.getNumChannels());
+		jassert(inBlock.getNumSamples() == outBlock.getNumSamples());
 
-            for (size_t c = 0; c < numChannels; ++c)
-            {
-                auto* src = inBlock.getChannelPointer(c);
-                auto* dst = outBlock.getChannelPointer(c);
-                            
-                const FloatType fb = feedback.getNextValue();
-                auto dly = dlyBufs->getSample(c, posR);
-                dst[pos] = src[pos] + (fb * dly);
+		auto bufSizeIn = inBlock.getNumSamples();
+		auto numChannels = inBlock.getNumChannels();
 
-                dlyBufs->setSample(c, posW, dst[pos]);
-            }            
-        }                    
-    }
+		if (context.isBypassed)
+		{
+			if (context.usesSeparateInputAndOutputBlocks())
+				outBlock.copyFrom(inBlock);
+
+			return;
+		}
+
+		int bufSizeDly = dlyBufs.getNumSamples();
+
+		for (size_t pos = 0; pos < bufSizeIn; ++pos, ++posR)
+		{
+			if (posR >= bufSizeDly)
+				posR = 0;
+
+			for (int c = 0; c < numChannels; ++c)
+			{
+				auto* src = inBlock.getChannelPointer(c);
+				auto* dst = outBlock.getChannelPointer(c);
+
+				const FloatType fb = feedback.getNextValue();
+				auto dly = dlyBufs.getSample(c, posR);
+				dst[pos] = src[pos] + (fb * dly);			
+			}
+		}
+
+		for (size_t pos = 0; pos < bufSizeIn; ++pos, ++posW)
+		{
+			if (posW >= bufSizeDly)
+				posW = 0;
+
+			for (int c = 0; c < numChannels; ++c)
+			{
+				auto* src = inBlock.getChannelPointer(c);
+				auto* dst = outBlock.getChannelPointer(c);											
+
+				dlyBufs.setSample(c, posW, dst[pos]);
+			}
+		}
+	}	
 
 private:
 
-    //==============================================================================
-    size_t delayInSmpls = 0;
-    FloatType sampleRate = SAMPLERATE_DEFAULT;
-    juce::SmoothedValue<FloatType> feedback = FEEDBACK_DEFAULT;
+	//==============================================================================
+	int delayInSmpls{ 0 };
+	FloatType sampleRate{ SAMPLERATE_DEFAULT };
+	juce::SmoothedValue<FloatType> feedback{ FEEDBACK_DEFAULT };
 
-    int numChnls = 0;
+	int numChnls{ 0 };
 
-    /// These vectors need to be replaced byt something that excepts FloatType
-    std::unique_ptr<juce::AudioBuffer<FloatType>> dlyBufs;
-    
-    int posR;
-    int posW;
+	juce::AudioBuffer<float> dlyBufs;
+
+	int posR;
+	int posW;
 };
